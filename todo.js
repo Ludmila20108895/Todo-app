d852e13c83819adb9751acd42d7786bd6462b43c
Ludmila20108895 <20108895@mail.wit.ie>
2025-04-08 22:13:17 +0100

    Initial commit - basic todo app

---
+++ todo.js
@@ -0,0 +1,27 @@
+let todoItems = [];
+
+function renderTodo(todo) {
+  const table = document.getElementById("todo-table");
+  const row = table.insertRow(-1);
+  const textCell = row.insertCell(0);
+  textCell.innerText = todo.text;
+  const dateCell = row.insertCell(1);
+  dateCell.innerText = todo.date;
+}
+
+function addTodo() {
+  const todoText = document.getElementById("todo-id").value;
+  const todo = {
+    text: todoText,
+    date: new Date().toLocaleString("en-IE"),
+  };
+  todoItems.push(todo);
+  renderTodo(todo);
+}
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
